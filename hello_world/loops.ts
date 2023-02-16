98a19ea4410128834efb60e3c018ada86cd4a967
naddiemathkour <naddiemathkour@gmail.com>
2023-02-16 18:11:36 -0500

    Create loops.ts
    Playing with loops

---
+++ hello_world/loops.ts
@@ -0,0 +1,27 @@
+for(let i = 1; i < 128; i<<=1){
+    console.log(i);
+}
+
+let i = 0;
+while(i < 500){
+    i++;
+    console.log(i);
+}
+
+console.log("Cringe");
+
+do{
+    i--;
+    console.log(i);
+}while(i > 0);
+
+console.clear();
+
+let arr: number[] = [];
+for(let i = 0; i < 10; i++){
+    arr[i] = i;
+}
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
