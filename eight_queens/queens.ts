8a4bf825528905d3a9cd77c9291ea8f24b3cc2b8
naddiemathkour <naddiemathkour@gmail.com>
2023-03-13 23:16:14 -0400

    Updates

---
+++ eight_queens/queens.ts
@@ -0,0 +1,13 @@
+function printBoard(board:number[][]){
+    for(let i = 0; i < 8; i++){
+        console.log(String(board[i]));
+    }
+}
+
+function generate_permutation(){
+    let permutation:number[][] = []
+    let bad_inputs:number[] = [0,1,2,3,4,5,6,7];
+    for(let i = 0; i < 8; i++){
+        
+    }
+}
